61f1eae5b96f44321f0f5f04edd24381ca61d95a
Steve Ripberger <steve.ripberger@zipscene.com>
2017-06-06 10:52:12 -0400

    Replace Pool#countCollisions with #getCollisionScore

--- test/unit/pool.js
+++ test/unit/pool.js
@@ -1,6 +1,18 @@
 const Pool = require('../../lib/pool');
+const sinon = require('sinon');
+const utils = require('../../lib/utils');
 
 describe('Pool', function() {
+	let sandbox;
+
+	beforeEach(function() {
+		sandbox = sinon.sandbox.create();
+	});
+
+	afterEach(function() {
+		sandbox.restore();
+	});
+
 	it('stores provided array of players', function() {
 		let players = [
 			{ tag: 'foo' },
@@ -18,26 +30,48 @@ describe('Pool', function() {
 		expect(pool.players).to.deep.equal([]);
 	});
 
-	describe('#countCollisions', function() {
+	describe('#getCollisionScore', function() {
 		let pool;
 
 		beforeEach(function() {
+			sandbox.stub(utils, 'getRegionCounts');
 			pool = new Pool([
-				{ region: 'foo' },
-				{ region: 'bar' },
-				{ region: 'baz' },
-				{ region: 'bar' },
-				{ region: 'baz' },
-				{ region: 'baz' }
+				{ tag: 'dude' },
+				{ tag: 'bro' }
 			]);
 		});
 
-		it('returns sum of regional repeats', function() {
-			expect(pool.countCollisions()).to.equal(3);
+		it('returns number of colliding regions', function() {
+			utils.getRegionCounts.returns({
+				foo: 2,
+				bar: 2,
+				baz: 1
+			});
+
+			let result = pool.getCollisionScore();
+
+			expect(utils.getRegionCounts).to.be.calledOnce;
+			expect(utils.getRegionCounts).to.be.calledOn(utils);
+			expect(utils.getRegionCounts).to.be.calledWith(pool.players);
+			expect(result).to.equal(2);
+		});
+
+		it('counts mutiple collisions using natural sums', function() {
+			utils.getRegionCounts.returns({
+				foo: 3, // 2 collisions: 1 + 2 = 3
+				bar: 4 // 3 collisions:  1 + 2 + 3 = 6
+			});
+
+			expect( pool.getCollisionScore()).to.equal(9);
 		});
 
 		it('ignores provided region, if any', function() {
-			expect(pool.countCollisions('baz')).to.equal(1);
+			utils.getRegionCounts.returns({
+				foo: 10, // should be ignored
+				bar: 5, // 4 collisions: 1 + 2 + 3 + 4 = 10
+			});
+
+			expect( pool.getCollisionScore('foo')).to.equal(10);
 		});
 	});
 });
