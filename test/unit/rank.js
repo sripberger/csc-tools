3da273fd7a8b00e8654f1d671cadcd8a0e28a754
Steve Ripberger <steve.ripberger@zipscene.com>
2017-06-12 11:56:28 -0400

    Add Rank #crossover and #mutate methods

--- test/unit/rank.js
+++ test/unit/rank.js
@@ -56,4 +56,44 @@ describe('Rank', function() {
 			expect(result.players).to.equal(shuffledPlayers);
 		});
 	});
+
+	describe('#crossover', function() {
+		it('returns offspring created using utils::pmx', function() {
+			let baz = { tag: 'baz' };
+			let qux = { tag: 'qux' };
+			let fooRank = new Rank([ { tag: 'foo' } ]);
+			let barRank = new Rank([ { tag: 'bar' } ]);
+			sandbox.stub(utils, 'pmx').returns([ [ baz ], [ qux ] ]);
+
+			let result = fooRank.crossover(barRank);
+
+			expect(utils.pmx).to.be.calledOnce;
+			expect(utils.pmx).to.be.calledOn(utils);
+			expect(utils.pmx).to.be.calledWith(fooRank.players, barRank.players);
+			expect(result).to.be.an.instanceof(Array);
+			expect(result).to.have.length(2);
+			expect(result[0]).to.be.an.instanceof(Rank);
+			expect(result[0].players).to.deep.equal([ baz ]);
+			expect(result[1]).to.be.an.instanceof(Rank);
+			expect(result[1].players).to.deep.equal([ qux ]);
+		});
+	});
+
+	describe('#mutate', function() {
+		it('returns mutated copy created using utils::reciprocalExchange', function() {
+			let foo = { tag: 'foo' };
+			let bar = { tag: 'bar' };
+			let rank = new Rank([ foo, bar ]);
+			let rate = 0.001;
+			sandbox.stub(utils, 'reciprocalExchange').returns([ bar, foo ]);
+
+			let result = rank.mutate(rate);
+
+			expect(utils.reciprocalExchange).to.be.calledOnce;
+			expect(utils.reciprocalExchange).to.be.calledOn(utils);
+			expect(utils.reciprocalExchange).to.be.calledWith(rank.players, rate);
+			expect(result).to.be.an.instanceof(Rank);
+			expect(result.players).to.deep.equal([ bar, foo ]);
+		});
+	});
 });
