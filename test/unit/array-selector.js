d4d05207e713feefe3800ce3f4c88460dee5ed74
Steve Ripberger <steve.ripberger@zipscene.com>
2017-06-30 10:47:55 -0400

    Add ArraySelector class

---
+++ test/unit/array-selector.js
@@ -0,0 +1,95 @@
+const ArraySelector = require('../../lib/array-selector');
+const Selector = require('../../lib/selector');
+const Individual = require('../lib/individual');
+const sinon = require('sinon');
+
+describe('ArraySelector', function() {
+	it('extends Selector', function() {
+		let selector = new ArraySelector();
+
+		expect(selector).to.be.an.instanceof(Selector);
+		expect(selector.settings).to.deep.equal({});
+	});
+
+	it('supports settings argument', function() {
+		let settings = { foo: 'bar' };
+
+		let selector = new ArraySelector(settings);
+
+		expect(selector.settings).to.equal(settings);
+	});
+
+	it('creates empty individuals array', function() {
+		let selector = new ArraySelector();
+
+		expect(selector.individuals).to.deep.equal([]);
+	});
+
+	describe('#add', function() {
+		let selector, foo, bar, baz;
+
+		beforeEach(function() {
+			selector = new ArraySelector();
+			foo = new Individual('foo');
+			bar = new Individual('bar');
+			baz = new Individual('baz');
+			selector.add(foo);
+		});
+
+		it('pushes provided individual onto individuals array', function() {
+			selector.add(bar);
+
+			expect(selector.individuals).to.deep.equal([ foo, bar ]);
+		});
+
+		it('supports multiple arguments', function() {
+			selector.add(bar, baz);
+
+			expect(selector.individuals).to.deep.equal([ foo, bar, baz ]);
+		});
+	});
+
+	describe('#getSize', function() {
+		it('returns length of individuals array', function() {
+			let selector = new ArraySelector();
+			let foo = new Individual('foo');
+			let bar = new Individual('bar');
+			let baz = new Individual('baz');
+			selector.add(foo, bar, baz);
+
+			expect(selector.getSize()).to.equal(3);
+		});
+	});
+
+	describe('#getBest', function() {
+		let selector;
+
+		beforeEach(function() {
+			selector = new ArraySelector();
+		});
+
+		it('returns highest-scoring individual', function() {
+			let foo = new Individual('foo');
+			let bar = new Individual('bar');
+			let baz = new Individual('baz');
+			sinon.stub(foo, 'getFitnessScore').returns(8);
+			sinon.stub(bar, 'getFitnessScore').returns(10);
+			sinon.stub(baz, 'getFitnessScore').returns(9);
+			selector.add(foo, bar, baz);
+
+			let result = selector.getBest();
+
+			expect(foo.getFitnessScore).to.be.called;
+			expect(foo.getFitnessScore).to.always.be.calledOn(foo);
+			expect(bar.getFitnessScore).to.be.called;
+			expect(bar.getFitnessScore).to.always.be.calledOn(bar);
+			expect(baz.getFitnessScore).to.be.called;
+			expect(baz.getFitnessScore).to.always.be.calledOn(baz);
+			expect(result).to.equal(bar);
+		});
+
+		it('returns null if generation is empty', function() {
+			expect(selector.getBest()).to.be.null;
+		});
+	});
+});
